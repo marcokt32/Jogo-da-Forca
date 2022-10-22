457a3f73d8e62c290677dcb4acc2a25a01000cca
Marco <quintelamarco94@gmail.com>
2022-10-21 21:34:29 -0300

    salvando ultimo estado funcional

--- js/canvas.js
+++ js/canvas.js
@@ -11,13 +11,13 @@ function desenharLinhas() {
     tabuleiro.fillStyle = 'blue';
     tabuleiro.strokeStyle = 'blue';
 
-    let largura = 600/palavraSecreta.length;
+    let largura = 1200/palavraSecreta.length;
 
     tabuleiro.beginPath();
     
     for(let i = 0; i < palavraSecreta.length; i++) {
-        tabuleiro.moveTo(310+(largura*i),70);
-        tabuleiro.lineTo(340+(largura*i),70);
+        tabuleiro.moveTo(400+(largura*i),70);
+        tabuleiro.lineTo(450+(largura*i),70);
     }
 
     tabuleiro.stroke();
@@ -25,17 +25,17 @@ function desenharLinhas() {
 }
 
 function escreverLetraCorreta(index) {
-    tabuleiro.font = 'bold 40px Inter';
+    tabuleiro.font = 'bold 80px Inter';
     tabuleiro.lineCap = 'round';
     tabuleiro.fillStyle = 'blue';
     tabuleiro.lineWidth = 6;
-    let largura = 600/palavraSecreta.length;
+    let largura = 1200/palavraSecreta.length;
 
     if(fim === false) {
         if(acertos < palavraSecreta.length) {
-            tabuleiro.fillText(palavraSecreta[index], 310+(largura*index), 50)
+            tabuleiro.fillText(palavraSecreta[index], 400+(largura*index), 65)
         }else{
-            tabuleiro.fillText(palavraSecreta[index], 310+(largura*index), 50)
+            tabuleiro.fillText(palavraSecreta[index], 400+(largura*index), 65)
             document.querySelector(".pop-up-ganhou").style.display = "block";
             document.querySelector(".tutorial").style.display = "none";
             let img = document.querySelector('.desenho-forca')
@@ -46,16 +46,16 @@ function escreverLetraCorreta(index) {
 }
 
 function escreverLetrasIncorretas(letra, erros) {
-    tabuleiro.font = 'bold 40px Inter';
+    tabuleiro.font = 'bold 50px Inter';
     tabuleiro.lineCap = 'round';
     tabuleiro.fillStyle = 'blue';
     tabuleiro.lineWidth = 6;
 
     if(fim === false) {
         if(erros < 6) {
-            tabuleiro.fillText(letra, 400 + (40 * erros), 130, 40)
+            tabuleiro.fillText(letra, 600 + (40 * erros), 130, 40)
         }else{
-            tabuleiro.fillText(letra, 400 + (40 * erros), 130, 40)
+            tabuleiro.fillText(letra, 600 + (40 * erros), 130, 40)
             document.querySelector(".pop-up-perdeu").style.display = "block";
             document.querySelector(".tutorial").style.display = "none";
             fim = true;
